48fa8a152c65ca844b223383397ebb344bcdfae9
Vasiliy Shubko <flit77@gmail.com>
2017-07-25 23:24:47 +0200

    upload func with cloudinary

--- src/components/Upload.js
+++ src/components/Upload.js
@@ -3,16 +3,29 @@ import { Link } from 'react-router';
 import Nav from './Nav';
 
 class Upload extends Component {
+
+  uploadWidget = () => {
+    window.cloudinary.openUploadWidget(
+      { cloud_name: cloud_name',
+        upload_preset: 'unsigned-preset',
+        tags: ['miniflix'],
+        sources: ['local', 'url', 'google_photos', 'facebook', 'image_search']
+      },
+      function(error, result) {
+          console.log("This is the result of the last upload", result);
+      });
+  }
+
   render() {
     return (
       <div>
         <Nav />
         <h3 className="text-center">Upload Your 20-second Video in a Jiffy</h3>
-        <hr />
+        <hr/>
 
         <div className="col-sm-12">
           <div className="jumbotron text-center">
-            <button className="btn btn-lg btn-info"> Upload Video</button>
+            <button onClick={this.uploadWidget} className="btn btn-lg btn-info"> Upload Video</button>
           </div>
         </div>
       </div>
